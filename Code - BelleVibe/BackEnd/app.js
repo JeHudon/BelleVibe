1980aec34464fccae03ca5182530a81f0d8b37b9
Jérôme Hudon <hudonj18@gmail.com>
2026-03-05 21:01:34 -0500

    App fonctionelle sur port 3000

--- Code - BelleVibe/BackEnd/app.js
+++ Code - BelleVibe/BackEnd/app.js
@@ -1,35 +1,48 @@
 /////////////////////////////////////// Création du serveur ////////////////////////////////////////////
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const crypto = require('crypto');
-
-/* Permet au serveur de traiter des données au format Json */
-app.use(express.json());
+const crypto = require("crypto");
 
 /* Path permet de gérer les chemins de fichiers */
-const path = require('path');
+const path = require("path");
 
 /* Importe la base de données de creationBd.js */
-// const { db, createTable } = require('./creationBd');
-const { default: knex } = require('knex');
+const { db, createTables } = require("./creationBd");
+const { default: knex } = require("knex");
+
+app.use(express.json());
 
 // Augmenter la limite pour les requêtes JSON (par défaut 100kb)
-app.use(express.json({ limit: '10mb' })); // accepte jusqu'à 10MB
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "10mb" })); // accepte jusqu'à 10MB
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // Ajout des en-têtes CORS pour permettre les requêtes depuis le frontend (car sinon faisait des erreurs de politique de même origine et envoyait des requêtes bloquées)
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
 // Servir les fichiers statiques
 app.use(express.static(path.join(__dirname, "../../"))); // client global
 app.use(express.static(path.join(__dirname, "../client"))); // client connexion-inscription
-app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // dossier pour les images uploadées
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // dossier pour les images uploadées
 
+app.get("/", (req, res) => {
+    res.send("Serveur fonctionne");
+});
 
+app.use("/notes", require("./notes.js"));
+
+createTables()
+    .then(() => {
+        const PORT = 3000;
+        app.listen(PORT, () => {
+            console.log(`Serveur démarré sur le port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Erreur lors de l'initialisation de la base de données:", err);
+    });
