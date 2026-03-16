368ea296601225e4fa0b70450b40a9165b7a1564
Alek Hanachian <alek.hanachian@gmail.com>
2026-03-16 11:13:19 -0400

    fonction authentifier

--- Code - BelleVibe/BackEnd/fonctionsCommunes.js
+++ Code - BelleVibe/BackEnd/fonctionsCommunes.js
@@ -1,3 +1,7 @@
+const jwt = require('jsonwebtoken')
+// clée ajoutée dans la logique de création des tokens 
+const jwt_mdp = "MECHANTE_belle_Vibe_2026"
+
 // Fonction de validation générique pour les champs d'une note
 // Exemple d'utilisation : 
 // validerChamps({ idDossier, idEmploye, type, titre, note })
@@ -10,4 +14,34 @@ function validerChamps(champs) {
     return true;
 }
 
-module.exports = { validerChamps };
+
+// fonction pour valider l'utilisateur qui est logged in
+// devrait être utilisé dans chaque action sauf pour log in, 
+// vérifier ensuite que l'utilisateur a les permissions de faire l'action demandée
+// exemple d'utilisation:
+// app.get('/taRoute', authentifier, (req, res) => {stuff})
+const authentifier = (req, res, next) => {
+    // récupère le header 'authorization'
+    const authHeader = req.headers['authorization']
+    // sépare le "Bearer {token}" en 2 (séparés par l'espace) 
+    // et garde seulement le token 
+    const token = authHeader && authHeader.split(' ')[1]
+    // erreur si pas de token
+    if (!token) {
+        return res.status(401).json({ message: "Accès refusé. Token manquant." })
+    }
+    try {
+        // décode le token et le renvoye dans la requête dans user
+        const userDecoded = jwt.verify(token, jwt_mdp)
+        req.user = userDecoded
+        next()
+    }
+    catch (error) {
+        return res.status(403).json({ message: "Token invalide ou expiré" })
+    }
+}
+
+
+
+
+module.exports = { validerChamps, authentifier };
