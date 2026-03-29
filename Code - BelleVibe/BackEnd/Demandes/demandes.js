3a6a2bcd57d303fd4587ae4ea9c09de26681e741
Raphaël Lemieux <lemieuxr4@gmail.com>
2026-03-29 12:56:29 -0400

    Finition routes demandes et collection Postman

--- Code - BelleVibe/BackEnd/Demandes/demandes.js
+++ Code - BelleVibe/BackEnd/Demandes/demandes.js
@@ -1,4 +1,4 @@
-const { validerChamps, authentifier, log } = require("../fonctionsCommunes");
+const { validerChamps, authentifier, log, authentifierSupp } = require("../fonctionsCommunes");
 const { db } = require("../BD/creationBd");
 
 const express = require("express");
@@ -7,7 +7,7 @@ const router = express.Router();
 // Routes pour la table demande
 
 // Get demande d'un dossier
-router.get("/getDemande/:idDossier", async (req, res) => {
+router.get("/getDemande/:idDossier", authentifier, async (req, res) => {
     const { idDossier } = req.params;
     try {
         const demande = await db("demandes").select("*").where("idDossier", idDossier)
@@ -17,9 +17,9 @@ router.get("/getDemande/:idDossier", async (req, res) => {
         res.status(500).json({ error: "Erreur serveur.." })
     } 
 })
-
+    
 // Get tout les demandes
-router.get("/getDemandes", async (req, res) => {
+router.get("/getDemandes", authentifier, async (req, res) => {
     try {
         const demandes = await db("demandes").select("*")
         res.status(200).json(demandes)
@@ -30,7 +30,7 @@ router.get("/getDemandes", async (req, res) => {
 })
 
 // Get les demandes ouvertes (pour dashboard)
-router.get("/getDemandesOuvertes", async (req, res) => {
+router.get("/getDemandesOuvertes", authentifier, async (req, res) => {
     try {
         const demandesOuvertes = await db("demandes").select("*").where("statutDemande", "Ouverte")
         res.status(200).json(demandesOuvertes)
@@ -41,7 +41,7 @@ router.get("/getDemandesOuvertes", async (req, res) => {
 })
 
 // Get les demandes en retard (pour dashboard superviseur)
-router.get("/getDemandesEnRetard", async (req, res) => {
+router.get("/getDemandesEnRetard", authentifier, async (req, res) => {
     try {
         const demandesEnRetard = await db("demandes").select("*").where("statutDemande", "En retard")
         res.status(200).json(demandesEnRetard)
@@ -52,7 +52,7 @@ router.get("/getDemandesEnRetard", async (req, res) => {
 })
 
 // Get les demandes en attente (pour dashboard)
-router.get("/getDemandesEnAttente", async (req, res) => {
+router.get("/getDemandesEnAttente", authentifier, async (req, res) => {
     try {
         const demandesEnAttente = await db("demandes").select("*").where("statutDemande", "En attente")
         res.status(200).json(demandesEnAttente)
@@ -63,7 +63,7 @@ router.get("/getDemandesEnAttente", async (req, res) => {
 })
 
 // Créé une demande
-router.post("/creerDemande/:idDossier", async (req, res) => {
+router.post("/creerDemande/:idDossier", authentifier, async (req, res) => {
     try {
 
         // Récupération des paramètres/body
@@ -97,3 +97,44 @@ router.post("/creerDemande/:idDossier", async (req, res) => {
 })
 
 // Modifier une demande
+router.put("/modifierDemande/:idDossier", authentifierSupp, async (req, res) => {
+    try {
+
+        // Récupération des paramètres/body
+        console.log(req.body)
+        const { typeDemande, statutDemande, noteInterne } = req.body
+        const { idDossier } = req.params
+
+        // Vérification que tous les champs sont remplis
+        const validationResult = validerChamps({ idDossier, typeDemande, statutDemande, noteInterne });
+        if (validationResult.error) {
+            return res.status(400).json({ error: validationResult.error });
+        }
+
+        // Vérifie que la demande existe
+        const verifDemande = await db("demandes").where("idDossier", idDossier).first()
+        if (!verifDemande) {
+            // Indique à l'utilisateur si la demande n'existe pas
+            return res.status(404).json({ error : "Demande inexistante" })
+        }
+
+        // On met les infos dans une variable
+        const demande = {
+            idDossier : idDossier,
+            typeDemande : typeDemande,
+            statutDemande : statutDemande,
+            noteInterne : noteInterne
+        }
+
+        // On update la demande correspondante dans la base de données
+        await db("demandes").where("idDossier", idDossier).update(demande)
+        await log(req.user.id, "UPDATE", "DEMANDES", Number(idDossier))
+        return res.status(201).json({ ...demande, idDossier: Number(idDossier) })
+
+    } catch (error) {
+        console.error("Erreur /editDemandee/:idDossier")
+        res.status(500).json({ error: "Erreur serveur.." }) 
+    }
+})
+
+module.exports = router;
