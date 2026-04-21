7ca8499a9e513441fe8776b64c221ca20bf71890
e6302453 <hudonj18@gmail.com>
2026-04-21 13:49:11 -0400

    Merge avant + ajout Details dans routeur

--- Code - BelleVibe/FrontEnd/src/Routeur.jsx
+++ Code - BelleVibe/FrontEnd/src/Routeur.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App.jsx";
-import { Login } from "./Login.jsx";
+import App from "./Pages/App/App.jsx";
+import DetailsCompte from "./Pages/DetailsCompte/DetailsCompte.jsx";
+import { Login } from "./Pages/Login/Login.jsx";
 
 export function Routeur() {
     return (
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/login" element={<Login />} />
+				<Route path="/comptes/:id/:onglet" element={<DetailsCompte />} />
             </Routes>
         </BrowserRouter>
     )
--- Code - BelleVibe/FrontEnd/src/main.jsx
+++ Code - BelleVibe/FrontEnd/src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "bulma/css/bulma.min.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { Routeur } from "./Routeur.jsx";
+import "bulma/css/bulma.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode >
     <Routeur />
-  </React.StrictMode>
+  </StrictMode>,
 );
