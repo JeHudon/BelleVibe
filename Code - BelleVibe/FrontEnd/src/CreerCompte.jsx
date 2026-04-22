df9b80e7aa38091e5cf01317f66a036cca8269fa
Raphaël Lemieux <lemieuxr4@gmail.com>
2026-04-22 15:35:00 -0400

    colonnes

--- Code - BelleVibe/FrontEnd/src/CreerCompte.jsx
+++ Code - BelleVibe/FrontEnd/src/CreerCompte.jsx
@@ -44,7 +44,7 @@ export function CreerCompte() {
         if (etape == 1) {
             return (
                 <div className="mt-2">
-                    <div className="title is-4">Sélectionner un client</div>
+                    <h3 className="title is-4">Sélectionner un client</h3>
                     <div className="subtitle is-5 mt-2">Choisissez le client pour ce nouveau compte</div>
                     {clients.map((client) => (
                         <div key={client.idClient}
@@ -71,30 +71,24 @@ export function CreerCompte() {
                 </div>
             )
         }
-        if (etape == 2) {
-            return (
-                <div className="mt-2">
-                    <div className="title is-4">Choisir les services</div>
-                    <div className="subtitle is-5 mt-2">Choisissez le client pour ce nouveau compte</div>
-                    {clients.map((client) => (
-                        <div key={client.idClient} className="button is-light" style={{ display: "flex", flexDirection: "column", width: "100%", height: "auto", marginBottom: "0.5rem", alignItems: "flex-start", textAlign: "left" }}>
-                            <div className="subtitle is-5 mb-0">{client.prenomClient} {client.nomClient}</div>
-                            <div className="subtitle is-6 mb-0">{client.telephoneClient} - {client.courrielClient}</div>
-                        </div>
-                    ))}
-                </div>
-            )
-        }
     }
 
     return (
-        <div className="container is-centered mt-3">
-            <div className="title">Créer un nouveau compte</div>
-            <div className="subtitle">Assistant de création du compte</div>
-            <div className="box">
-                {affichageSelonEtape(etape)}
+        <div className="section">
+            <div className="container is-centered mt-3">
+                <h1 className="title">Créer un nouveau compte</h1>
+                <h2 className="subtitle is-5 is-spaced">Assistant de création du compte</h2>
+                <div className="columns is-centered">
+                    <div className="column is-4">Étape 1</div>
+                    <div className="column is-4">Étape 2</div>
+                    <div className="column is-4">Étape 3</div>
+                    <div className="column is-4">Étape 4</div>
+                </div>
+                <div className="box">
+                    {affichageSelonEtape(etape)}
+                </div>
+                <button className="button is-dark" onClick={() => { setEtape(etape + 1) }}>Suivant</button>
             </div>
-            <button className="button is-dark" onClick={() => { setEtape(etape + 1) }}>Suivant</button>
         </div>
     )
 } 
